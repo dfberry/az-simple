7119261dcbc0751c84571dc563b1d5bef9123b52
4ttedertlf <107864453+4ttedertlf@users.noreply.github.com>
2022-11-06 15:40:28 -0800

    0.0.1 - test publish

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Azure-sample
+
+## Services
+
+### Key Vault
+
+#### Secrets
+
+##### Authentication
+
+* DefaultAzureCredential - you must provide environment mechanism to authenticate such as environment variables
---
+++ src/azure/key-vault-secret.ts
@@ -0,0 +1,28 @@
+const { SecretClient } = require("@azure/keyvault-secrets");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+class KeyVaultSecret {
+
+  keyVaultName: string;
+  keyVaultUrl: string;
+  keyVaultSdkClient: any;
+
+  constructor(keyVaultName: string){
+    this.keyVaultName = keyVaultName;
+
+    this.keyVaultUrl = "https://" + keyVaultName + ".vault.azure.net";
+    this.keyVaultSdkClient = new SecretClient(this.keyVaultUrl, new DefaultAzureCredential());
+  }
+    
+  async getSecret(secretName: string){
+    return await this.keyVaultSdkClient.getSecret(secretName);
+  }
+  async setSecret(secretName: string, secretValue:string){
+    return await this.keyVaultSdkClient.setSecret(secretName, secretValue);
+  }
+  async deleteSerect(secretName:string){
+    return await this.keyVaultSdkClient.beginDeleteSecret(secretName);
+  }
+}
+
+export default KeyVaultSecret;
